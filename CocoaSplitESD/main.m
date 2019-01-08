842188940fd4812be7da4af88d274a155df96e4a
Zakk <zakk@rsdio.com>
2019-01-08 00:02:08 -0500

    Initial Commit

---
+++ CocoaSplitESD/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CocoaSplitESD
+//
+//  Created by Zakk on 1/8/19.
+//  Copyright © 2019 Zakk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
